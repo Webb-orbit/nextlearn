3cc20fe5e0bf087b1867289b51e359b6b3a06c03
SAYAN <sayanhumanist@gmail.com>
2024-03-25 13:07:39 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , bsdcsdcsdcsd fvsdf dfvd 
+            , bsdcsdcsdcsd fvvd 
           </p>
           <Link
             href="/login"
